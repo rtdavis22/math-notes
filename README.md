f38a15983efd327d63d252f67d9c4cb12cced717
Robert Davis <rtdavis22@gmail.com>
2020-10-26 12:20:35 -0700

    Revert to serving static HTML

--- README.md
+++ README.md
@@ -3,10 +3,14 @@ https://robs-math-notes.herokuapp.com/
 ### Build
 
 To generate node_modules/:
+
 npm install
 
-To start up development server
+To start up development server:
+
 npm start
+
+More info: https://create-react-app.dev/
 
 ### Deploy
 
--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
-import Proof from './math/proof.js';
 
 function App() {
-  return (
+/*
+  var math = '\\[ \\textrm{im }f = \\{ f(g) \\mid g \\in G. \\} \\]';
+
+  var test = (
     <div>
-      This is a div.
-      <Proof>
-        <span>sfsdfsdf</span>
-      </Proof>
+      \[ x_{33} = 3 \]
+      \(\{'{'}e\{'}'}\)
+      <span dangerouslySetInnerHTML={{__html: '\\(\\{e\\}\\)'}} />
+      <div>
+        This is a div.
+        <Proof>
+          <span>sfsdfsdf</span>
+        </Proof>
+        \(x \mid 2\)
+      </div>
+      <span dangerouslySetInnerHTML={{__html: math}} />
     </div>
   );
+
+  var test2 = <span dangerouslySetInnerHTML={{__html: '$$ a_{ij} \\pi $$'}} />;
+*/
+  return null;
 }
 
 export default App;
